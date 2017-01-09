57ed79f6fedd40d1cf614fc18f0aaf5f03bbd2a3
emancypage <szymkowiak.szymon@gmail.com>
2017-01-09 19:25:03 +0100

    nieskonczone menu Szymek - dokoncze

---
+++ JanuszeProgramowania/skrypty.js
@@ -0,0 +1,7 @@
+var lbTags = document.getElementByTagName("li");
+
+for (var i = 0; i<=lbTags.lenght; i++) {
+	lbTags[i].addEventListener("onMouseOver", function(e){
+		document.getElementById()
+	} )
+}
